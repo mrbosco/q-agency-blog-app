1856a159654240a514afe6ec5433c61e1a61dbb8
Alem Tatarević <alem.tatarevic@edu.fit.ba>
2023-11-21 18:37:10 +0100

    chore: configure ESLint and Prettier for code quality and consistency

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const currentDir = (metaUrl: string): string => {
+  return path.dirname(fileURLToPath(metaUrl));
+};
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslint()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(currentDir(import.meta.url), 'src'),
+      },
+    ],
+  },
 });
